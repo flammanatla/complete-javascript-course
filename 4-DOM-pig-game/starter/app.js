c42d04110905ccc168d970d6e23a030f98b707f8
flamma.natla <flamma.natla@gmail.com>
2019-07-27 00:09:16 +0100

    task3 coding challenge 6

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -10,19 +10,19 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer, gamePlaying;
-var goal = 20;
+var goal =520;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
+		document.querySelector('.notification').style.display = 'none';
+
 		//Random number
 		var dice = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
 
 		//display correct img
 		var diceDOM = document.querySelectorAll('.dice');
-		// diceDOM.style.display = 'block';
-		// diceDOM.src = 'dice-' + dice + '.png';
 
 		diceDOM.forEach(function(die, index) {
 			die.style.display = 'block';
@@ -30,8 +30,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		});
 
 		//update the round score IF rolled number != 1
-		if (dice !== 1) {
-			roundScore += dice;
+		if (dice[0] !== 1 && dice[1] !== 1) {
+			roundScore += dice[0] + dice[1];
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
 			if (roundScore >= goal) {
 				announceTheWinner();
@@ -50,7 +50,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		//update the UI
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-		//isWinner();
 		//check if active player won the game
 		if (scores[activePlayer] >= goal) {
 			announceTheWinner();
@@ -62,46 +61,12 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
-function announceTheWinner() {
-	document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
-
-	hideDice();
-
-	document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-
-	gamePlaying = false;
-}
-
-function nextPlayer() {
-	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-	roundScore = 0;
-
-	document.getElementById('current-0').textContent = 0;
-	document.getElementById('current-1').textContent = 0;
-
-	document.querySelector('.player-0-panel').classList.toggle('active');
-	document.querySelector('.player-1-panel').classList.toggle('active');
-
-	hideDice();
-
-};
-
-function hideDice() {
-	var allDice = document.querySelectorAll('.dice');
-	
-	allDice.forEach(function(die) {
-		die.style.display = 'none';
-	});
-}
-
 function init() {
 	scores = [0, 0];
 	roundScore = 0;
 	activePlayer = 0; 
 	gamePlaying = true;
 
-	// document.querySelector('#current-' + activePlayer).textContent = dice;
 	// document.querySelector('#current-' +activePlayer).innerHTML = '<em>' + dice + '</em';
 
 	//document.querySelector('.dice').style.display = 'none';
@@ -115,6 +80,8 @@ function init() {
 	document.querySelector('#name-0').textContent = 'Player 1';
 	document.querySelector('#name-1').textContent = 'Player 2';
 
+	document.querySelector('.notification').style.display = 'none';
+
 	//since we don't know who is the winner, we remove winner and active css styles from both panels 
 	document.querySelector('.player-0-panel').classList.remove('winner');
 	document.querySelector('.player-1-panel').classList.remove('winner');
@@ -123,4 +90,40 @@ function init() {
 
 	//make player 1 active on the beginning of the game
 	document.querySelector('.player-0-panel').classList.add('active');
+};
+
+function hideDice() {
+	var allDice = document.querySelectorAll('.dice');
+	
+	allDice.forEach(function(die) {
+		die.style.display = 'none';
+	});
+};
+
+function nextPlayer() {
+	//add a notification that activeplayer is losing the turn
+	document.querySelector('.notification').innerHTML = 'End of turn for player ' + (activePlayer + 1);
+	document.querySelector('.notification').style.display = 'block';
+
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = 0;
+	document.getElementById('current-1').textContent = 0;
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	//hideDice();
+};
+
+function announceTheWinner() {
+	document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+
+	hideDice();
+
+	document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+	gamePlaying = false;
 }
